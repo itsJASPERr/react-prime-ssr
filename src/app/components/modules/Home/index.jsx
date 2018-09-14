c1481b9a6ad9048102ab6875866ebdce7d000120
Sander <sander.vispoel@labela.nl>
2018-09-14 14:22:45 +0200

    Add Redux example functionality

--- src/app/components/modules/Home/index.jsx
+++ src/app/components/modules/Home/index.jsx
@@ -1,16 +1,22 @@
+/* eslint-disable */
 import React, { Component } from 'react';
+import PT from 'prop-types';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { install } from 'ducks/demo';
 import Button from 'common/Button';
 import { Container, Header, Logo } from './styled';
 
 class Home extends Component {
-  // - These parameters are given to the method and shown as example
+  // - These parameters are given to the method by default
   // eslint-disable-next-line no-unused-vars
   static async getInitialProps({ req, res, match, history, location, ...ctx }) {
     return { whatever: 'stuff' };
   }
 
   render() {
+    const { demo } = this.props;
+
     return (
       <Container>
         <Header>
@@ -19,11 +25,28 @@ class Home extends Component {
         </Header>
         <Link to="/about">About</Link>
         <div>
-          <Button>Install</Button>
+          {demo.passed ? (
+            <h1>Installed succesfully!</h1>
+          ) : (
+            <Button onClick={this.props.install}>
+              {demo.loading ? 'Installing...' : 'Install'}
+            </Button>
+          )}
         </div>
       </Container>
     );
   }
 }
 
-export default Home;
+Home.propTypes = {
+  install: PT.func,
+  demo: PT.shape({
+    install: PT.func,
+  }),
+};
+
+const mapStateToProps = state => ({
+  demo: state.demo,
+});
+
+export default connect(mapStateToProps, { install })(Home);
